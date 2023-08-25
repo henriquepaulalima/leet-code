14f46771dc15d9e7495dd7999a9366e162bcee16
henriquepaulalima <henrique.lima@fluna.io>
2023-08-25 08:03:19 -0300

    Add 876

---
+++ 876-Middle-of-the-Linked-List.ts
@@ -0,0 +1,53 @@
+/*
+
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+
+*/
+
+
+// Definition for singly-linked list.
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+
+function middleNode(head: ListNode | null): ListNode | null {
+  let middleNode: ListNode | null = head;
+  let endNode = head;
+
+  while (endNode != null && endNode.next != null) {
+    if(middleNode != null) middleNode = middleNode.next;
+    endNode = endNode.next.next;
+  }
+
+  return middleNode;
+};
+
