4b909fdbb3a36e9feb18f2eab4c0b03998ac9a0d
henriquepaulalima <henrique.lima@fluna.io>
2023-09-27 13:00:07 -0300

    Add 931

---
+++ Dynamic_Programming/931-Minimum-Falling-Path-Sum.ts
@@ -0,0 +1,41 @@
+/*
+
+Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+
+A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+Output: 13
+Explanation: There are two falling paths with a minimum sum as shown.
+Example 2:
+
+
+Input: matrix = [[-19,57],[-40,-5]]
+Output: -59
+Explanation: The falling path with a minimum sum is shown.
+ 
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 100
+-100 <= matrix[i][j] <= 100
+
+*/
+
+function minFallingPathSum(matrix: number[][]): number {
+  let memo = matrix[0];
+
+  for (let i = 1; i < matrix.length; i++) {
+    memo = matrix[i].map((val, j) => {
+      return Math.min(memo[j], memo[j - 1] ?? 10001, memo[j + 1] ?? 10001) + val;
+    });
+  }
+
+  return memo.reduce((a, c) => Math.min(a, c), memo[0]);
+};
