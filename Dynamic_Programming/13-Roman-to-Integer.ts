f279bc4f12a546f32af784e43fd5591b88ab7971
henriquepaulalima <henrique.p.lima0@gmail.com>
2024-02-21 08:14:15 -0300

    Add 13-Roman-to-Integer

---
+++ Dynamic_Programming/13-Roman-to-Integer.ts
@@ -0,0 +1,117 @@
+/*
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+ 
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+*/
+
+function romanToInt(s: string): number {
+    const characters = s.split('');
+    let result = 0;
+
+    characters.forEach((char, index) => {
+        switch (char) {
+            case 'I':
+                switch (characters[index + 1]) {
+                    case 'V':
+                        result = result + 4;
+                        break;
+                    case 'X':
+                        result = result + 9;
+                        break;
+                    default:
+                        result = result + 1;
+                        break;
+                }
+                break;
+            case 'V':
+                if (characters[index - 1] == 'I') return;
+                result = result + 5;
+                break;
+            case 'X':
+                if (characters[index - 1] == 'I') return;
+                switch (characters[index + 1]) {
+                    case 'L':
+                        result = result + 40;
+                        break;
+                    case 'C':
+                        result = result + 90;
+                        break;
+                    default:
+                        result = result + 10;
+                        break;
+                }
+                break;
+            case 'L':
+                if (characters[index - 1] == 'X') return;
+                result = result + 50;
+                break;
+            case 'C':
+                if (characters[index - 1] == 'X') return;
+                switch (characters[index + 1]) {
+                    case 'D':
+                        result = result + 400;
+                        break;
+                    case 'M':
+                        result = result + 900;
+                        break;
+                    default:
+                        result = result + 100;
+                        break;
+                }
+                break;
+            case 'D':
+                if (characters[index - 1] == 'C') return;
+                result = result + 500;
+                break;
+            case 'M':
+                if (characters[index - 1] == 'C') return;
+                result = result + 1000;
+                break;
+            default:
+        };
+    });
+
+    return result;
+};
